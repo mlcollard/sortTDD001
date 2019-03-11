2b2912f9358e81e9ea86351ace54ee063eb68c7a
Michael L. Collard, Ph.D <collard@uakron.edu>
2019-03-11 13:11:08 -0400

    Sort a vector of four elements

--- sort.cpp
+++ sort.cpp
@@ -36,4 +36,14 @@ void sort(std::vector<int>& v) {
 
     order(v[0], v[1]);
 
+    if (v.size() <= 3)
+        return;
+
+    order(v[2], v[3]);
+
+    order(v[0], v[1]);
+
+    order(v[1], v[2]);
+
+    order(v[0], v[1]);
 }
